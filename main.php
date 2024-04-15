5b9f7441fb15aeed321ca7663bcded3215154c84
Miguel dos Santos Miranda <117553721+Miguelkkkk@users.noreply.github.com>
2024-04-15 16:59:15 -0300

    :)

--- main.php
+++ main.php
@@ -1,4 +1,9 @@
 <?php
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    // Exibir mensagem de erro e interromper a execução
+    echo 'Acesso inválido! Utilize o método POST para enviar o formulário.';
+    exit;
+}
 $nome = $_POST['name'];
 $email = $_POST['email'];
 $mensagem = $_POST['mensagem'];
@@ -6,3 +11,4 @@ $mensagem = $_POST['mensagem'];
 echo "Nome: $nome<br>";
 echo "Email: $email<br>";
 echo "Mensagem: $mensagem";
+
