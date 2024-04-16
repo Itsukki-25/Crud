c36c39b6a53fd08c5d2265b1b4876e05017319ce
Miguel dos Santos Miranda <117553721+Miguelkkkk@users.noreply.github.com>
2024-04-15 22:02:34 -0300

    complete commit

--- main.php
+++ main.php
@@ -1,14 +1,30 @@
 <?php
+global $mysqli;
+include('conexao.php');
+
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     // Exibir mensagem de erro e interromper a execução
     echo 'Acesso inválido! Utilize o método POST para enviar o formulário.';
     exit;
 }
-$nome = $_POST['nome'];
+
+$name = $_POST['name'];
 $email = $_POST['email'];
 $mensagem = $_POST['mensagem'];
 
-echo "Nome: $nome<br>";
-echo "Email: $email<br>";
-echo "Mensagem: $mensagem";
+//echo "Nome: $nome<br>";
+//echo "Email: $email<br>";
+//echo "Mensagem: $mensagem";
 
+if (isset($_POST['email']) || isset($_POST['nome'])) {
+    if (strlen($_POST['email']) == 0) {
+        echo "Preencha seu email";
+    }
+    else if (strlen($_POST['name']) == 0) {
+        echo "Preencha seu nome";
+    }
+    else {
+        $sql_code = "INSERT INTO users(name,email,message) VALUES('$name','$email','$mensagem')";
+        $sql_query = mysqli_query($mysqli, $sql_code) or die('Falha na execução do código SQL: ' . mysqli_error($mysqli));
+    }
+}
